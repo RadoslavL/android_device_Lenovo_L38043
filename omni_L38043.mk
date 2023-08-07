86563a5d3edc1d3d232afe2d79727dc4bf15b148
RadoslavL <rlelchev@abv.bg>
2023-08-07 21:19:36 +0300

    Add all the files

---
+++ omni_L38043.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/Lenovo/L38043/prebuilt/kernel:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := L38043
+PRODUCT_NAME := omni_L38043
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := L38043
+PRODUCT_MANUFACTURER := Lenovo
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="Lenovo/L38043/K9:8.1.0/O11019/1575368252:user/release-keys" \
+    PRIVATE_BUILD_DESC="full_lava6762_l01-user 8.1.0 O11019 1575368252 release-keys"
