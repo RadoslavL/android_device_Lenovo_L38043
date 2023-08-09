57018588fe9b06e189d649327e6f362a40d3410b
RadoslavL <rlelchev@abv.bg>
2023-08-09 23:33:24 +0300

    Fixed the issue (probably)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := device/Lenovo/L38043
 
-TARGET_BOARD_PLATFORM := mt6765               # From ro.mediatek.platform, but lowercase value
+# From ro.mediatek.platform, but lowercase value
+TARGET_BOARD_PLATFORM := mt6765
 TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := L38043        # From ro.product.board
+# From ro.product.board
+TARGET_BOOTLOADER_BOARD_NAME := L38043
 
 # These two are for MTK Chipsets only
 BOARD_USES_MTK_HARDWARE := true
@@ -10,30 +12,42 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true            # To add info about F2FS Filesystem Data Block
+# To add info about F2FS Filesystem Data Block
+TARGET_USERIMAGES_USE_F2FS := true
 # Put The Size of your Recovery Partition below, get it from your "MT****_Android_scatter.txt"
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
 # BOARD_USES_FULL_RECOVERY_IMAGE := true      # Uncomment this line if you want to remove size restriction
-BOARD_FLASH_BLOCK_SIZE := 0                   # Might be different for your chip
-BOARD_HAS_NO_REAL_SDCARD := true              # Depricated
+# Might be different for your chip
+BOARD_FLASH_BLOCK_SIZE := 131072
+# Depricated
+BOARD_HAS_NO_REAL_SDCARD := true
 # BOARD_HAS_NO_SELECT_BUTTON := true          # Depricated
 BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_NO_MISC_PARTITION := true           # Delete if your partition table has /misc
+# Delete if your partition table has /misc
+BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
-RECOVERY_SDCARD_ON_DATA := true               # Optional: If /sdcard partition is emulated on /data partition 
+# Optional: If /sdcard partition is emulated on /data partition 
+RECOVERY_SDCARD_ON_DATA := true
 
 # TWRP stuff
-TW_EXCLUDE_SUPERSU := true                    # true/false: Add SuperSU or not
-TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption Support or not
-TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
+# true/false: Add SuperSU or not
+TW_EXCLUDE_SUPERSU := true
+# true/false: Add Data Encryption Support or not
+TW_INCLUDE_CRYPTO := true
+# Optional: Disables virtual mouse
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
-DEVICE_RESOLUTION := 720x1440                 # The Resolution of your Device
-TARGET_SCREEN_HEIGHT := 1440                    # The height
-TARGET_SCREEN_WIDTH := 720                      # The width
+# Set the exact theme you wanna use. If resulation doesn't match, define the height/width
+TW_THEME := portrait_hdpi
+# The Resolution of your Device
+DEVICE_RESOLUTION := 720x1440
+# The height
+TARGET_SCREEN_HEIGHT := 1440
+# The width
+TARGET_SCREEN_WIDTH := 720
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 # Set the Brightness Control File Path below (as per your chip/device)
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
@@ -42,13 +56,19 @@ TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backl
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
+# Set custom brightness, low is better
+TW_DEFAULT_BRIGHTNESS := 80
 
-TW_INCLUDE_NTFS_3G := true                    # Include NTFS Filesystem Support
-TW_INCLUDE_FUSE_EXFAT := true                 # Include Fuse-ExFAT Filesystem Support
-TWRP_INCLUDE_LOGCAT := true                   # Include LogCat Binary
-TW_INCLUDE_FB2PNG := true                     # Include Screenshot Support
-TW_DEFAULT_LANGUAGE := en                     # Set Default Language 
+# Include NTFS Filesystem Support
+TW_INCLUDE_NTFS_3G := true
+# Include Fuse-ExFAT Filesystem Support
+TW_INCLUDE_FUSE_EXFAT := true
+# Include LogCat Binary
+TWRP_INCLUDE_LOGCAT := true
+# Include Screenshot Support
+TW_INCLUDE_FB2PNG := true
+# Set Default Language
+TW_DEFAULT_LANGUAGE := en
 TW_EXTRA_LANGUAGES := false
 
 # Kernel
@@ -65,8 +85,9 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x14f88000 --tags_offset 0x13f88000
 # Set FSTAB
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
 
-TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
-TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
+# Remove if the device is 32-bit
+TARGET_BOARD_SUFFIX := _64                    
+TARGET_USES_64_BIT_BINDER := true             
 
 # Architecture
 # According to the device's architecture (64-bit or 32-bit)
@@ -75,12 +96,14 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53             # Change the value to "generic" if build fails suddenly due to arch error
+# Change the value to "generic" if build fails suddenly due to arch error
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53         # Change the value to "generic" if build fails suddenly due to arch error
+# Change the value to "generic" if build fails suddenly due to arch error
+TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
@@ -89,6 +112,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7              # Change the value to "generic" if build fails suddenly due to arch error
+# Change the value to "generic" if build fails suddenly due to arch error
+TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI),$(TARGET_CPU_ABI2)
 endif
